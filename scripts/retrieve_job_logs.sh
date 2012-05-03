79cdcffb251d395c40cfa729160d8f4eab5faa0c
Your Name <you@example.com>
2012-05-03 19:51:52 +0100

    script added to retrieve all logs for a particular job

---
+++ scripts/retrieve_job_logs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SCRIPT:  retrieve_job_logs.sh
+#PURPOSE: Retrieve logs for a particular job from all machines
+
+FILENAME=$1
+JOBID=$2
+
+CIEL_DIR=/root/ciel_data
+LOG_DIR=$CIEL_DIR/logs
+JOB_LOG_DIR=$CIEL_DIR/jobs
+JOB_FILENAME=$JODID.log
+
+while read MACHINE
+do
+      echo "Retrieving log from machine $MACHINE for job $JOBID"
+      ssh -n $MACHINE "cat $LOG_DIR/$MACHINE.log | grep $JOBID" >> $JOB_LOG_DIR/$JOB_FILENAME
+
+done < $FILENAME
