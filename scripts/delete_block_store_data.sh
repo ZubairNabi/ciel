503a5361cad3c1729db75d05eb5f5476d08755e9
Your Name <you@example.com>
2012-05-04 12:01:09 +0100

    Scripts added to delete data from block store

---
+++ scripts/delete_block_store_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SCRIPT:  delete_block_store_data.sh
+#PURPOSE: Delete block store data on each worker
+
+FILENAME=$1
+USERNAME=root
+
+CIEL_DIR=/root/ciel_data
+BS_DIR=$CIEL_DIR/block_store
+BS_DATA_DIR=$BS_DIR/data
+
+while read MACHINE
+do
+      echo "Deleting block store data on container $MACHINE"
+      ssh -n $MACHINE "cd $BS_DATA_DIR; rm -f i * .*;"
+
+done < $FILENAME
