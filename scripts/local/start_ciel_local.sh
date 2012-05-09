e89de4b21e2e71ff5f3e41920ba1606381653011
Your Name <you@example.com>
2012-05-09 02:57:32 +0100

    More startup scripts

--- scripts/local/start_ciel_local.sh
+++ scripts/local/start_ciel_local.sh
@@ -4,6 +4,7 @@
 
 FILENAME_MASTER=$1
 FILENAME_WORKERS=$2
+FILENAME_WORKERS2=$3
 USERNAME=root
 
 MOUNT_DIR=/mnt/vfs
@@ -14,7 +15,8 @@ BS_DIR=$CIEL_DIR/block_store
 JJ_DIR=$CIEL_DIR/job_journal
 
 MASTER_PORT=8000
-WORKER_PORT=8002
+WORKER_PORT=54000
+WORKER_PORT2=50000
 NUM_THREADS=1
 
 while read MASTER
@@ -28,8 +30,17 @@ done < $FILENAME_MASTER
 while read MACHINE
 do
       echo "Starting CIEL worker on $MACHINE"
-      ciel worker --master="http://localhost:$MASTER_PORT" --port="$WORKER_PORT" --num-threads="$NUM_THREADS" --pidfile="$MOUNT_DIR$MACHINE$PID_DIR/$MACHINE" --daemonise --logfile="$MOUNT_DIR$MACHINE$LOG_DIR/$MACHINE.log" --blockstore="$MOUNT_DIR$MACHINE$BS_DIR"
+      ciel worker --master="http://localhost:$MASTER_PORT" --port="$WORKER_PORT" --num-threads="$NUM_THREADS" --pidfile="$MOUNT_DIR$MACHINE$PID_DIR/$MACHINE" --daemonise --logfile="$MOUNT_DIR$MACHINE$LOG_DIR/$MACHINE.log"
       let "WORKER_PORT += 1"
       sleep 1
 
 done < $FILENAME_WORKERS
+
+while read MACHINE2
+do
+      echo "Starting CIEL worker on $MACHINE2"
+      ciel worker --master="http://localhost:$MASTER_PORT" --port="$WORKER_PORT2" --num-threads="$NUM_THREADS" --pidfile="$MOUNT_DIR$MACHINE2$PID_DIR/$MACHINE2" --daemonise --logfile="$MOUNT_DIR$MACHINE2$LOG_DIR/$MACHINE2.log"
+      let "WORKER_PORT2 += 1"
+      sleep 1
+
+done < $FILENAME_WORKERS2
