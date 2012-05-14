1556e53f24b9f94248d93fd0f0fce55d69ad31ab
Your Name <you@example.com>
2012-05-14 17:08:09 +0100

    Script added to parse worker logs for errors

---
+++ scripts/search_errors_worker_logs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SCRIPT:  search_errors_worker_logs.sh
+#PURPOSE: Search for errors in the log of each worker
+
+FILENAME=$1
+USERNAME=root
+
+#CIEL_DIR=/root/ciel_data
+CIEL_DIR=/mnt/ciel_data
+LOG_DIR=$CIEL_DIR/logs
+
+while read MACHINE
+do
+      echo "Tailing log of $MACHINE"
+      ssh -n $MACHINE cat $LOG_DIR/$MACHINE.log | egrep 'error|Exception|failure'
+
+done < $FILENAME
