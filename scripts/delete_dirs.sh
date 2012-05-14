dbd452e9f5e8e33ce4153929f7ea4bee1d5f446f
Your Name <you@example.com>
2012-05-14 14:47:28 +0100

    Script added to delete directory structure

---
+++ scripts/delete_dirs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SCRIPT:  delete_dirs.sh
+#PURPOSE: Delete CIEL directories on each host
+
+FILENAME=$1
+USERNAME=root
+
+CIEL_DIR=/mnt/ciel_data
+#CIEL_DIR=/root/ciel_data
+
+while read MACHINE
+do
+      echo "Deleting directories on container $MACHINE"
+      ssh -n $MACHINE "rm -Rf $CIEL_DIR"
+
+done < $FILENAME
