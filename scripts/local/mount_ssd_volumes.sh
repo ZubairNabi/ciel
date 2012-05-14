f23aaa75e9bebadd061a580b0976986b956c2fc3
Your Name <you@example.com>
2012-05-14 19:39:12 +0100

    Number of containers increased to 41

--- scripts/local/mount_ssd_volumes.sh
+++ scripts/local/mount_ssd_volumes.sh
@@ -7,7 +7,7 @@ FILENAME=$1
 while read MACHINE
 do
       echo "Mounting SSDs $MACHINE"
-      mount -t ext3 /dev/ciel_ssd_vg/lv_ssd_$MACHINE /mnt/ssd$MACHINE
+      mount -t ext3 /dev/ciel_ssd_vg/ssd_lvm$MACHINE /mnt/ssd$MACHINE
       sleep 1
 
 done < $FILENAME
