ebb3059f50a4a1eb351649c90d15d3bf72cbd1d1
Your Name <you@example.com>
2012-05-15 13:52:07 +0100

    delete_block_store_results script now also cleans CIEL_DIR/tmp

--- scripts/delete_block_store_results.sh
+++ scripts/delete_block_store_results.sh
@@ -9,10 +9,11 @@ USERNAME=root
 CIEL_DIR=/mnt/ciel_data
 BS_DIR=$CIEL_DIR/block_store
 BS_DATA_DIR=$BS_DIR/data
+TEMP_DIR=$CIEL_DIR/tmp
 
 while read MACHINE
 do
       echo "Deleting results in block store on container $MACHINE"
-      ssh -n $MACHINE "cd $BS_DATA_DIR; ls -a | grep -v upload | xargs rm"
+      ssh -n $MACHINE "cd $BS_DATA_DIR; ls -a | grep -v upload | xargs rm; rm -Rf "$TEMP_DIR"; mkdir $TEMP_DIR"
 
 done < $FILENAME
