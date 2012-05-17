4907d99f56675bcb6dcea50c319eff8c0864e7f2
Your Name <you@example.com>
2012-05-17 18:05:50 +0100

    delete_block_store_data_local now deletes the entire block store

--- scripts/local/delete_block_store_data_local.sh
+++ scripts/local/delete_block_store_data_local.sh
@@ -11,9 +11,8 @@ while read MACHINE
 do
       CIEL_DIR=$ROOT_DIR/$MACHINE/ciel_data
       BS_DIR=$CIEL_DIR/block_store
-      BS_DATA_DIR=$BS_DIR/data
       echo "Deleting block store data on container $MACHINE"
-      rm -Rf $BS_DATA_DIR
-      mkdir $BS_DATA_DIR
+      rm -Rf $BS_DIR
+      mkdir $BS_DIR
 
 done < $FILENAME
